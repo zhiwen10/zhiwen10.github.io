d5faadf55c881a78c6778fd094f585aaee5f3a36
zhiwen10 <yezhiwen@uw.edu>
2025-01-19 00:28:24 +0000

    Deploying to gh-pages from @ zhiwen10/zhiwen10.github.io@3245511a6b354cf262480afab4643eac163965e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,9 +55,9 @@ ninja.data = [{
           window.location.href = "/blog/2024/photo-gallery/";
         
       },
-    },{id: "post-zhiwen-ye",
+    },{id: "post-zhiwen-ye-ph-d",
       
-        title: 'Zhiwen Ye <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        title: 'Zhiwen Ye, Ph.D. <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
       description: "A simple, whitespace theme for academics. Based on [*folio](https://github.com/bogoli/-folio) design.",
       section: "Posts",
